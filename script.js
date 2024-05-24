48f1421328fd36fec23b5d4117af997462986be1
Moeen Heydari <moeentk@gmail.com>
2024-05-24 08:56:17 +0200

    240524: The function to shuffle the options added

--- script.js
+++ script.js
@@ -35,3 +35,47 @@ document
         console.log(quizQuestions);
         alert("Quiz question submitted successfully!");
     });
+
+// Collect all radio button elements within the option containers
+const radioButtons = Array.from(
+    document.querySelectorAll('.option-container input[type="radio"]')
+);
+// Collect all text input elements within the option containers
+const optionInputs = Array.from(
+    document.querySelectorAll('.option-container input[type="text"]')
+);
+
+// Function to shuffle the options
+function randomizeOptions() {
+    // Extracting values from option inputs
+    let options = optionInputs.map((input) => input.value);
+
+    // Maps over radio buttons and returns true if the radio button is checked
+    let radios = radioButtons.map((radio) => radio.checked);
+
+    // Create an array of indices, in this case [0, 1, 2, 3]
+    let indices = options.map((_, index) => index); // The underscore _ is used here to indicate that this parameter is not needed or used in the function
+
+    // Shuffle the indices array with Fisher-Yates shuffle algorithm
+    for (let i = indices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [indices[i], indices[j]] = [indices[j], indices[i]];
+    }
+
+    // Create new arrays for shuffled options and radios
+    let shuffledOptions = indices.map((index) => options[index]);
+    let shuffledRadios = indices.map((index) => radios[index]);
+
+    // Assigning shuffled values back to option inputs
+    optionInputs.forEach((input, index) => {
+        input.value = shuffledOptions[index];
+    });
+
+    // Assigning shuffled radio buttons back
+    radioButtons.forEach((radio, index) => {
+        radio.checked = shuffledRadios[index];
+    });
+
+    // Update background color after randomizing
+    updateBackgroundColor();
+}
